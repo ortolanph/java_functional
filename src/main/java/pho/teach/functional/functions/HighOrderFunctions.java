20d675179ee1975cb2a569e7f04e3f16364ea122
Paulo Henrique Ortolan <portolan@mercadona.es>
2025-01-21 15:14:57 +0100

    I have another idea for HOF

--- src/main/java/pho/teach/functional/functions/HighOrderFunctions.java
+++ src/main/java/pho/teach/functional/functions/HighOrderFunctions.java
@@ -1,16 +1,6 @@
 package pho.teach.functional.functions;
 
-import pho.teach.functional.functions.hof.Displacement;
-
 public class HighOrderFunctions {
-
-    public double simpleTorricelli(double vi, double a, double di, double df) {
-        return Math.sqrt(Math.pow(vi, 2) + 2 * a * (df - di));
-    }
-
-    public double simpleTorricelli(double vi, double a, Displacement displacement) {
-        return Math.sqrt(Math.pow(vi, 2) + 2 * a * displacement.delta());
-    }
 
 
 }
--- src/main/java/pho/teach/functional/functions/hof/Displacement.java
+++
@@ -1,7 +0,0 @@
-package pho.teach.functional.functions.hof;
-
-public record Displacement(double start, double finish) {
-    public double delta() {
-        return finish - start;
-    }
-}
