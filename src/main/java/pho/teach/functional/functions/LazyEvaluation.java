18b98205f437a8d2fc0cb0646dcf1a094afee432
Paulo Ortolan <paulo.ortolan@gmail.com>
2025-05-10 14:47:14 +0100

    Code changes

--- src/main/java/pho/teach/functional/functions/LazyEvaluation.java
+++ src/main/java/pho/teach/functional/functions/LazyEvaluation.java
@@ -9,7 +9,7 @@ import pho.teach.functional.commons.entities.functions.Store;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Predicate;
+import java.util.function.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,13 +21,13 @@ public class LazyEvaluation {
         this.market = market;
     }
 
-    protected Stream<Country> countries() {
+    protected Stream<Country> countriesStream() {
         return market
                 .getCountries()
                 .stream();
     }
 
-    protected Stream<Country> filteredCountries(String countryName) {
+    protected Stream<Country> filteredCountriesStream(String countryName) {
         return
                 market
                         .getCountries()
@@ -35,7 +35,7 @@ public class LazyEvaluation {
                         .filter(country -> country.getName().equals(countryName));
     }
 
-    protected Stream<Country> filteredCountries(Predicate<Country> countryPredicate) {
+    protected Stream<Country> filteredCountriesStream(Predicate<Country> countryPredicate) {
         return
                 market
                         .getCountries()
@@ -43,20 +43,25 @@ public class LazyEvaluation {
                         .filter(countryPredicate);
     }
 
-    protected Stream<Store> stores(Stream<Country> countryStream) {
+    protected Stream<Store> storeStream(Stream<Country> countryStream) {
         return countryStream
                 .map(Country::getStores)
                 .flatMap(List::stream);
     }
 
+    protected Stream<String> mappedStore(Predicate<Country> countryPredicate, Function<Store, String> storeFunction) {
+        return storeStream(filteredCountriesStream(countryPredicate))
+            .map(storeFunction);
+    }
+
     protected Stream<Store> filteredStores(String countryName) {
-        return filteredCountries(countryName)
+        return filteredCountriesStream(countryName)
                 .map(Country::getStores)
                 .flatMap(List::stream);
     }
 
     protected Stream<Store> filteredStores(Predicate<Country> countryPredicate) {
-        return filteredCountries(countryPredicate)
+        return filteredCountriesStream(countryPredicate)
                 .map(Country::getStores)
                 .flatMap(List::stream);
     }
@@ -74,60 +79,66 @@ public class LazyEvaluation {
     }
 
     public List<String> allCountries() {
-        return countries()
+        return countriesStream()
                 .map(Country::getName)
                 .sorted()
                 .toList();
     }
 
     public List<String> allStores() {
-        return stores(countries())
+        return storeStream(countriesStream())
                 .map(Store::getId)
                 .sorted()
                 .toList();
     }
 
     public List<String> storesByCountry(String countryName) {
-        return stores(filteredCountries(countryName))
+        return storeStream(filteredCountriesStream(countryName))
                 .map(Store::getId)
                 .sorted()
                 .toList();
     }
 
     public List<String> storesByCountry(Predicate<Country> countryPredicate) {
-        return stores(filteredCountries(countryPredicate))
+        // 🗺
+        return storeStream(filteredCountriesStream(countryPredicate))
                 .map(Store::getId)
                 .sorted()
                 .toList();
     }
 
+    public List<String> storesByCountryFormatted(Predicate<Country> countryPredicate, Function<Store, String> storeFunction) {
+        return mappedStore(countryPredicate, storeFunction)
+            .toList();
+    }
+
     public Set<String> allSections() {
-        return sections(stores(countries()))
+        return sections(storeStream(countriesStream()))
                 .map(Section::getSection)
                 .collect(Collectors.toSet());
 
     }
 
     public Double totalRevenues() {
-        return revenues(sections(stores(countries())))
+        return revenues(sections(storeStream(countriesStream())))
                 .mapToDouble(Month::getRevenue)
                 .sum();
     }
 
     public Double totalRevenuesBySection(String sectionName) {
-        return revenues(sections(stores(countries())).filter(section -> section.getSection().equals(sectionName)))
+        return revenues(sections(storeStream(countriesStream())).filter(section -> section.getSection().equals(sectionName)))
                 .mapToDouble(Month::getRevenue)
                 .sum();
     }
 
     public Double totalRevenuesBySection(Predicate<Section> sectionPredicate) {
-        return revenues(sections(stores(countries())).filter(sectionPredicate))
+        return revenues(sections(storeStream(countriesStream())).filter(sectionPredicate))
                 .mapToDouble(Month::getRevenue)
                 .sum();
     }
 
     public Double totalRevenuesByCountryAndSection(String countryName, String sectionName) {
-        return revenues(sections(stores(filteredCountries(countryName)))
+        return revenues(sections(storeStream(filteredCountriesStream(countryName)))
                 .filter(section -> section.getSection().equals(sectionName)))
                 .mapToDouble(Month::getRevenue)
                 .sum();
@@ -136,8 +147,8 @@ public class LazyEvaluation {
     public Double totalRevenuesByCountryAndSection(Predicate<Country> countryPredicate, Predicate<Section> sectionPredicate) {
         return revenues(
                 sections(
-                        stores(
-                                countries()
+                        storeStream(
+                                countriesStream()
                                         .filter(countryPredicate)
                         )
                 ).filter(sectionPredicate))
@@ -147,7 +158,7 @@ public class LazyEvaluation {
 
     public Map<String, Double> getCountrySummary() {
         return
-                countries()
+                countriesStream()
                         .collect(Collectors.toMap(
                                 Country::getName,
                                 country -> revenues(sections(country.getStores().stream()))
@@ -158,7 +169,7 @@ public class LazyEvaluation {
 
     public Map<String, Double> getOneCountrySummary(String countryName) {
         return
-                countries()
+                countriesStream()
                         .filter(country -> country.getName().equals(countryName))
                         .collect(Collectors.toMap(
                                 Country::getName,
@@ -170,7 +181,7 @@ public class LazyEvaluation {
 
     public Map<String, Double> getOneCountrySummary(Predicate<Country> countryPredicate) {
         return
-                countries()
+                countriesStream()
                         .filter(countryPredicate)
                         .collect(Collectors.toMap(
                                 Country::getName,
@@ -181,7 +192,7 @@ public class LazyEvaluation {
     }
 
     public Map<String, Double> getStoreSummary() {
-        return stores(countries())
+        return storeStream(countriesStream())
                 .collect(Collectors.toMap(
                         Store::getId,
                         store -> revenues(store.getSections().stream())
@@ -191,7 +202,7 @@ public class LazyEvaluation {
     }
 
     public Map<String, List<String>> getCountryStores() {
-        return countries()
+        return countriesStream()
                 .collect(Collectors.toMap(
                         Country::getName,
                         country -> country
--- src/main/java/pho/teach/functional/functions/LazyEvaluationMain.java
+++ src/main/java/pho/teach/functional/functions/LazyEvaluationMain.java
@@ -1,13 +1,12 @@
 package pho.teach.functional.functions;
 
-import pho.teach.functional.commons.entities.functions.Country;
-import pho.teach.functional.commons.entities.functions.Market;
+import pho.teach.functional.commons.entities.functions.*;
 import pho.teach.functional.commons.loader.RevenueLoader;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class LazyEvaluationMain {
 
@@ -26,12 +25,12 @@ public class LazyEvaluationMain {
             System.out.printf(" * %s%n", s);
         });
 
-//        List<String> stores = evaluator.allStores();
-//
-//        System.out.printf("%nAll Stores:%n%n");
-//        stores.forEach(s -> {
-//            System.out.printf(" * %s%n", s);
-//        });
+        List<String> stores = evaluator.allStores();
+
+        System.out.printf("%nAll Stores:%n%n");
+        stores.forEach(s -> {
+            System.out.printf(" * %s%n", s);
+        });
 
         List<String> storesInBrazil = evaluator.storesByCountry("Brazil");
 
@@ -49,33 +48,43 @@ public class LazyEvaluationMain {
             System.out.printf(" * %s%n", s);
         });
 
-//        Set<String> sections = evaluator.allSections();
-//
-//        System.out.printf("%nAll Sections:%n%n");
-//        sections.forEach(s -> {
-//            System.out.printf(" * %s%n", s);
-//        });
-//
-//        Double totalRevenue = evaluator.totalRevenues();
-//        System.out.printf("%nTotal Revenue: %,.2f%n", totalRevenue);
-//
-//        Map<String, Double> countrySummary = evaluator.getCountrySummary();
-//        System.out.printf("%nRevenue by Country:%n%n");
-//        countrySummary.forEach((key, value) -> {
-//            System.out.printf(" * %s: %,.2f%n", key, value);
-//        });
-//
-//        Map<String, Double> storeSummary = evaluator.getStoreSummary();
-//        System.out.printf("%nRevenue by Store:%n%n");
-//        storeSummary.forEach((key, value) -> {
-//            System.out.printf(" * %s: %,.2f%n", key, value);
-//        });
-//
-//        Map<String, List<String>> countryStores = evaluator.getCountryStores();
-//        System.out.printf("%nStores by Countries:%n%n");
-//        countryStores.forEach((key, value) -> {
-//            System.out.printf(" * %s: %s%n", key, value);
-//        });
+        Function<Store, String> storeFunction = store -> "%s - %s - %s".formatted(store.getId(), store.getLocation(), store.isDelivery());
+
+        List<String> formattedStoresInBrazilAndArgentina = evaluator
+            .storesByCountryFormatted(brazilPredicate.or(argentinaPredicate), storeFunction);
+
+        System.out.printf("%nAll Stores in Brazil and Argentina:%n%n");
+        formattedStoresInBrazilAndArgentina.forEach(s -> {
+            System.out.printf(" * %s%n", s);
+        });
+
+        Set<String> sections = evaluator.allSections();
+
+        System.out.printf("%nAll Sections:%n%n");
+        sections.forEach(s -> {
+            System.out.printf(" * %s%n", s);
+        });
+
+        Double totalRevenue = evaluator.totalRevenues();
+        System.out.printf("%nTotal Revenue: %,.2f%n", totalRevenue);
+
+        Map<String, Double> countrySummary = evaluator.getCountrySummary();
+        System.out.printf("%nRevenue by Country:%n%n");
+        countrySummary.forEach((key, value) -> {
+            System.out.printf(" * %s: %,.2f%n", key, value);
+        });
+
+        Map<String, Double> storeSummary = evaluator.getStoreSummary();
+        System.out.printf("%nRevenue by Store:%n%n");
+        storeSummary.forEach((key, value) -> {
+            System.out.printf(" * %s: %,.2f%n", key, value);
+        });
+
+        Map<String, List<String>> countryStores = evaluator.getCountryStores();
+        System.out.printf("%nStores by Countries:%n%n");
+        countryStores.forEach((key, value) -> {
+            System.out.printf(" * %s: %s%n", key, value);
+        });
     }
 
 }
